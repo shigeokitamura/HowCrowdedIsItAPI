dddd4ff91c3d3e2017dbfb816e256dc4aad105a3
Shigeo Kitamura <shigeo9462@gmail.com>
2020-05-05 01:51:35 +0000

    fix cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '*path' => 'options_request#preflight', via: :options
+
   namespace "api" do
     namespace "v1" do
       resources :restaurants, param: :restaurant_id, path: "/restaurants"
